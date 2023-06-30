b1c8921a2dd8255aac9db71ec453e53d77a77b6c
Carl <cboettig@gmail.com>
2023-06-30 17:56:44 +0000

    ray

--- ray_train.py
+++ ray_train.py
@@ -1,27 +1,23 @@
 from envs import one_fish
-#from ray.rllib.algorithms import ppo
-from ray.rllib.algorithms import td3
+from ray.rllib.algorithms import ppo, td3
 from ray.tune import register_env
 import os
 import pandas as pd
 import numpy as np
 import torch
 
-
 register_env("one_fish",one_fish.one_fish)
 
-## We could call env directly without this if only  our envs took a env_config dict argument
+#config = td3.TD3Config()
 
-config = td3.TD3Config()
-
-#config = ppo.PPOConfig()
+config = ppo.PPOConfig()
 config = config.resources(num_gpus=torch.cuda.device_count())
 config.framework_str="torch"
 config.create_env_on_local_worker = True
 config.env="one_fish"
 agent = config.build()
 
-run_id = "TD3"
+run_id = "PPO"
 iterations = 200
 checkpoint = (f"run{run_id}"+"/checkpoint_000{}".format(iterations))
 
