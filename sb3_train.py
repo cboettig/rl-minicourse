e003d42d5fdb2917f1dbe8a41f64c9ccb655a75f
Carl Boettiger <cboettig@gmail.com>
2023-07-05 06:27:38 +0000

    2k iter

--- sb3_train.py
+++ sb3_train.py
@@ -8,5 +8,5 @@ vec_env = make_vec_env(env, n_envs=4)
 log = os.path.expanduser("~/ray_results/sb3/")
 
 model = ARS("MlpPolicy", vec_env, verbose=0, tensorboard_log=log) # , device="cpu")
-model.learn(total_timesteps=300_000, progress_bar=True)
+model.learn(total_timesteps=200_000, progress_bar=True)
 model.save("ars_fish")
